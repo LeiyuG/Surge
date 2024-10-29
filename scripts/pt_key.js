c44f6f62690bc7512be65ffe0fcad7cc30c52f58
LeiyuG <41772908+LeiyuG@users.noreply.github.com>
2024-10-29 23:06:57 +0800

    Update pt_key.js

--- scripts/pt_key.js
+++ scripts/pt_key.js
@@ -69,8 +69,8 @@ var jdCookie = CV.match(/pt_pin=.+?;/) + CV.match(/pt_key=.+?;/);
     ]);
 
     // 关闭模块，自需要执行一次
-    await $httpAPI("POST", "v1/modules", { ["pt_key"]: false }, () => $done());
-    $notification.post("pt_key", "", "Update success!");
+    await $httpAPI("POST", "v1/modules", { ["京东cookie"]: false }, () => $done());
+    $notification.post("京东cookie", "", "Update success!");
     $done();
 })();
 
